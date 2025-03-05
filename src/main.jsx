d0fe959af8764201c85288bf089c14267d33fb84
faryd ignacio ortíz <faryd257@gmail.com>
2025-03-05 18:14:04 -0300

    progreso

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import AppRouter from "./router/AppRouter.jsx";
+import { AppRouter } from './router/AppRouter.jsx'
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
