780fa2ff31854cb1cafb388f31e830f44dd94fb1
faryd ignacio ortíz <faryd257@gmail.com>
2025-03-05 18:02:47 -0300

    actual

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import { AppRouter } from './router/AppRouter.JSX'
+import AppRouter from "./router/AppRouter.jsx";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
